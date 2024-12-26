a46abb8c0ea8e242e9fe791b86c7d8065e5d9246
Adrian Gookool <amgookool@hotmail.com>
2024-12-26 11:29:11 -0400

    🔧 Updated tailwind and svelte configs

--- apps/tic-tac-toe/tailwind.config.ts
+++ apps/tic-tac-toe/tailwind.config.ts
@@ -1,6 +1,7 @@
 import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
-
+import daisyui from 'daisyui';
+import { iconsPlugin, getIconCollections } from '@egoist/tailwindcss-icons';
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
@@ -8,5 +9,19 @@ export default {
 		extend: {}
 	},
 
-	plugins: [typography]
+	plugins: [typography, iconsPlugin({
+		collections: getIconCollections(["lucide", "mdi"])
+	}), daisyui],
+
+	// daisyUI config (optional - here are the default values)
+	daisyui: {
+		themes: true, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+		darkTheme: 'dark', // name of one of the included themes for dark mode
+		base: true, // applies background color and foreground color for root element by default
+		styled: true, // include daisyUI colors and design decisions for all components
+		utils: true, // adds responsive and modifier utility classes
+		prefix: '', // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+		logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+		themeRoot: ':root' // The element that receives theme color CSS variables
+	}
 } satisfies Config;
