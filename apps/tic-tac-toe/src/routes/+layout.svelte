1931155b0326f96ba87520b9d1c7278fd0df8eff
Adrian Gookool <amgookool@hotmail.com>
2024-12-26 11:32:41 -0400

    ✨ Updated content for routes

--- apps/tic-tac-toe/src/routes/+layout.svelte
+++ apps/tic-tac-toe/src/routes/+layout.svelte
@@ -3,4 +3,8 @@
 	let { children } = $props();
 </script>
 
+<svelte:head>
+	<title>Tic-Tac-Toe</title>
+</svelte:head>
+
 {@render children()}
--- apps/tic-tac-toe/src/routes/+page.svelte
+++ apps/tic-tac-toe/src/routes/+page.svelte
@@ -1,2 +1,56 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import ThemeController from '$lib/components/ThemeController.svelte';
+	import { PlayersState } from '$lib/states/players.svelte';
+
+    let playerOName = '';
+    let playerXName = '';
+
+    const handlePlayerNames = (event: SubmitEvent) => {
+        event.preventDefault();
+
+        PlayersState.setPlayerName('O', playerOName);
+        PlayersState.setPlayerName('X', playerXName);
+
+        goto('/game');
+    };
+
+</script>
+
+<main class="flex min-h-screen items-center justify-center">
+	<div class="card bg-base-100 mx-auto my-auto w-96 shadow-xl">
+		<div class="card-body gap-4">
+			<h2 class="card-title">Tic Tac Toe!</h2>
+			<form class="space-y-3" onsubmit={handlePlayerNames}>
+				<label class="input input-bordered fill-accent hover:fill-primary flex items-center gap-2">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						class="size-6 fill-inherit opacity-70"
+					>
+						<path
+							d="M18.3 5.71a1 1 0 00-1.42 0L12 10.59 7.12 5.71a1 1 0 00-1.42 1.42L10.59 12l-4.89 4.88a1 1 0 001.42 1.42L12 13.41l4.88 4.89a1 1 0 001.42-1.42L13.41 12l4.89-4.88a1 1 0 000-1.42z"
+						/>
+					</svg>
+					<input bind:value={playerXName} name="playerX" type="text" class="grow" placeholder="Name of Player X" />
+				</label>
+
+				<label class="input input-bordered fill-accent hover:fill-primary flex items-center gap-2">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						class="size-6 fill-inherit opacity-70"
+					>
+						<path
+							d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"
+						/>
+					</svg>
+					<input bind:value={playerOName}  name="playerO" type="text" class="grow" placeholder="Name of Player O" />
+				</label>
+				<div class="card-actions justify-center">
+					<button type="submit" class="btn btn-primary w-full text-xl font-semibold">Play!</button>
+				</div>
+			</form>
+		</div>
+	</div>
+</main>
