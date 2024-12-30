222cfc4bf47d8beadc98183c1c0431296c44ee96
Adrian Gookool <amgookool@hotmail.com>
2024-12-29 22:38:02 -0400

    ✨ Add about section with personal details and social links

---
+++ apps/portfolio/src/content.ts
@@ -0,0 +1,39 @@
+export const about = {
+    name: 'Adrian Gookool',
+    title: 'Software Engineer',
+    emails: ['aggookool@gmail.com', 'amgookool@hotmail.com'],
+    phone: '(868) 475-5372',
+    location: 'Trinidad and Tobago',
+    summary: 'Software Engineer with a passion for creating and developing software solutions. I am a highly motivated individual with a strong work ethic and a desire to learn and grow. I am always looking for new challenges and opportunities to expand my knowledge and skills.',
+    socials: [
+        {
+            name: 'LinkedIn',
+            url: 'https://www.linkedin.com/in/adrian-gookool/',
+        },
+        {
+            name: 'GitHub',
+            url: 'https://github.com/amgookool',
+        },
+    ],
+    languages: ['English'],
+    interests: ['Gaming', 'Music', 'Programming'],
+    hobbies: ['Coding', 'Gaming', 'Reading'],
+    references: [
+        {
+            name: 'John Doe',
+            position: 'Software Engineer',
+            company: 'Company',
+            email: 'test@domain.com'
+        },
+    ]
+};
+
+export const education = [];
+
+export const skills = [];
+
+export const certifications = [];
+
+export const workExperience = [];
+
+export const projects = [];
