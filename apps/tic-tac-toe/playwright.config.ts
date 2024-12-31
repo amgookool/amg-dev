152d592d9bf966a9430d104421175400f2e6722e
Adrian Gookool <amgookool@hotmail.com>
2024-12-31 08:11:05 -0400

    ✨ Add end-to-end tests and improve Playwright configuration for better test coverage

--- apps/tic-tac-toe/playwright.config.ts
+++ apps/tic-tac-toe/playwright.config.ts
@@ -1,10 +1,28 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
+	name: 'Tic Tac Toe',
+	reporter: 'html',
+	retries: 2,
 	webServer: {
-		command: 'npm run build && npm run preview',
+		command: 'pnpm run build && pnpm run preview',
 		port: 8788
 	},
+	testDir: 'e2e',
+	use: {
+		// Capture screenshot after each test failure.
+		screenshot: 'only-on-failure',
 
-	testDir: 'e2e'
+		// Record trace only when retrying a test for the first time.
+		trace: 'on-first-retry',
+
+		// Record video only when retrying a test for the first time.
+		video: 'on-first-retry',
+
+		// Emulates the user timezone.
+		timezoneId: 'America/La_Paz',
+
+		 // Viewport used for all pages in the context.
+		 viewport: { width: 1920, height: 1080 },
+	}
 });
