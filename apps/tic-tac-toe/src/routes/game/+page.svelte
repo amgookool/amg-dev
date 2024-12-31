197943f1852e1a41bbdc10afb736d945780a62c4
Adrian Gookool <amgookool@hotmail.com>
2024-12-31 08:10:15 -0400

    ♻️ Refactor game layout: replace main tag with section for improved semantics

--- apps/tic-tac-toe/src/routes/game/+page.svelte
+++ apps/tic-tac-toe/src/routes/game/+page.svelte
@@ -1,12 +1,9 @@
 <script lang="ts">
-	import { PlayersState } from '$lib/states/players.svelte';
 	import Board from '$lib/components/Board.svelte';
 	import ScoreCard from '$lib/components/ScoreCard.svelte';
-	$inspect(PlayersState.O);
-	$inspect(PlayersState.X);
 </script>
 
-<main class="flex w-full flex-col items-center justify-center gap-8 my-12">
+<section class="flex w-full flex-col items-center justify-center gap-8">
 	<ScoreCard />
 	<Board />
-</main>
+</section>
