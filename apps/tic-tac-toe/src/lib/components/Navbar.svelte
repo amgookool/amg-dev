70cfc1c58caf40989b748dfd03dfea5d754c020e
Adrian Gookool <amgookool@hotmail.com>
2025-01-03 22:53:46 -0400

    ✨ Enhance Navbar title with typewriter animation effect

--- apps/tic-tac-toe/src/lib/components/Navbar.svelte
+++ apps/tic-tac-toe/src/lib/components/Navbar.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <nav class="container flex w-full items-center justify-between px-8 py-2">
-    <h1 class="text-accent text-4xl">Tic Tac Toe</h1>
+    <h1 class={["motion-preset-typewriter-[11] motion-duration-[20000ms]", 'text-4xl text-accent']}>Tic Tac Toe</h1>
     <ThemeController />
 </nav>
