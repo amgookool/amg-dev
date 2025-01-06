0901b0307fd6b20533a639ecc976c16b6c1729f5
Adrian Gookool <amgookool@hotmail.com>
2025-01-06 08:19:59 -0400

    ✨ Update Navbar styles for improved text visibility and animation effects

--- apps/tic-tac-toe/src/lib/components/Navbar.svelte
+++ apps/tic-tac-toe/src/lib/components/Navbar.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-    import ThemeController from '$lib/components/ThemeController.svelte';
+	import ThemeController from '$lib/components/ThemeController.svelte';
 </script>
 
 <nav class="container flex w-full items-center justify-between px-8 py-2">
-    <h1 class={["motion-preset-typewriter-[11] motion-duration-[20000ms]", 'text-4xl text-accent']}>Tic Tac Toe</h1>
-    <ThemeController />
+	<h1 class={['text-4xl/normal text-info lg:motion-preset-float-sm']}>Tic Tac Toe</h1>
+	<ThemeController />
 </nav>
