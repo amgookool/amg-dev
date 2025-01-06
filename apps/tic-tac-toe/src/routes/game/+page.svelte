e6bfb978fd5164036b1b7502cccd88cba66e82f7
Adrian Gookool <amgookool@hotmail.com>
2025-01-06 08:19:08 -0400

    ✨ Add slide transition effect to game section for smoother UI experience

--- apps/tic-tac-toe/src/routes/game/+page.svelte
+++ apps/tic-tac-toe/src/routes/game/+page.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
 	import Board from '$lib/components/Board.svelte';
 	import ScoreCard from '$lib/components/ScoreCard.svelte';
+	import { slide } from 'svelte/transition';
 </script>
 
-<section class="flex w-full flex-col items-center justify-center gap-8">
+<section
+	transition:slide={{ duration: 500, axis: 'y', delay: 100 }}
+	class="flex w-full flex-col items-center justify-center gap-8"
+>
 	<ScoreCard />
 	<Board />
 </section>
