c7e8c14231b83b767a6b3b784905b4a3e62229be
Adrian Gookool <amgookool@hotmail.com>
2025-01-06 08:19:29 -0400

    🔧 Remove unused theme change functionality from layout component

--- apps/tic-tac-toe/src/routes/+layout.svelte
+++ apps/tic-tac-toe/src/routes/+layout.svelte
@@ -1,14 +1,8 @@
 <script lang="ts">
 	import Navbar from '$lib/components/Navbar.svelte';
-	import { onMount } from 'svelte';
-	import { themeChange } from 'theme-change';
 	import '../app.css';
-	
-	let { children } = $props();
 
-	onMount(() => {
-		themeChange(false);
-	});
+	let { children } = $props();
 </script>
 
 <svelte:head>
