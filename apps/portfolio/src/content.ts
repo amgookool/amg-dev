0f9afc49750cbf20d503957e3917b69156cb9e43
Adrian Gookool <amgookool@hotmail.com>
2025-01-07 23:01:29 -0400

    📝 Update portfolio content with enhanced personal details, education, skills, and work experience

--- apps/portfolio/src/content.ts
+++ apps/portfolio/src/content.ts
@@ -1,39 +1,255 @@
 export const about = {
-    name: 'Adrian Gookool',
-    title: 'Software Engineer',
-    emails: ['aggookool@gmail.com', 'amgookool@hotmail.com'],
-    phone: '(868) 475-5372',
-    location: 'Trinidad and Tobago',
-    summary: 'Software Engineer with a passion for creating and developing software solutions. I am a highly motivated individual with a strong work ethic and a desire to learn and grow. I am always looking for new challenges and opportunities to expand my knowledge and skills.',
-    socials: [
-        {
-            name: 'LinkedIn',
-            url: 'https://www.linkedin.com/in/adrian-gookool/',
-        },
-        {
-            name: 'GitHub',
-            url: 'https://github.com/amgookool',
-        },
-    ],
-    languages: ['English'],
-    interests: ['Gaming', 'Music', 'Programming'],
-    hobbies: ['Coding', 'Gaming', 'Reading'],
-    references: [
-        {
-            name: 'John Doe',
-            position: 'Software Engineer',
-            company: 'Company',
-            email: 'test@domain.com'
-        },
-    ]
+  name: "Adrian Gookool",
+  title: "Software Engineer",
+  emails: ["aggookool@gmail.com", "amgookool@hotmail.com"],
+  phone: "+1 (868) 475-5372",
+  city: "St. Augustine",
+  location: "Trinidad and Tobago",
+  summary:
+    "Software Engineer with a passion for creating and developing software solutions. I am a highly motivated and detail-oriented individual with a strong work ethic and a desire to learn and grow. I am always looking for new challenges and opportunities to expand my knowledge and skills.",
+  socials: [
+    {
+      name: "LinkedIn",
+      url: "https://www.linkedin.com/in/adrian-gookool-88a430198",
+    },
+    {
+      name: "GitHub",
+      url: "https://github.com/amgookool",
+    },
+  ],
+  languages: ["English"],
+  interests: ["Gaming", "Music", "Programming"],
+  hobbies: ["Coding", "Gaming", "Reading"],
+  references: [
+    {
+      name: "John Doe",
+      position: "Software Engineer",
+      company: "Company",
+      email: "test@domain.com",
+    },
+  ],
 };
 
-export const education = [];
+export const education = [
+  {
+    institution: "The University of the West Indies",
+    degree: "BSc. Electrical and Computer Engineering",
+    start: "2019",
+    end: "2023",
+    location: "St. Augustine, Trinidad and Tobago",
+    gpa: "3.47",
+    honors: "Upper Second Class Honors",
+  },
+  {
+    institution: "Trinity College East Sixth Form",
+    degree: "Caribbean Advanced Proficiency Examination (CAPE) Unit 2",
+    start: "2016",
+    end: "2017",
+    location: "Trincity, Trinidad and Tobago",
+    grades: [
+      {
+        subject: "Pure Mathematics",
+        grade: "I",
+      },
+      {
+        subject: "Applied Mathematics",
+        grade: "I",
+      },
+      {
+        subject: "Physics",
+        grade: "II",
+      },
+      {
+        subject: "Caribbean Studies",
+        grade: "II",
+      },
+    ],
+  },
+  {
+    institution: "Trinity College East",
+    degree: "Caribbean Advanced Proficiency Examination (CAPE) Unit 1",
+    start: "2015",
+    end: "2016",
+    location: "Trincity, Trinidad and Tobago",
+    grades: [
+      {
+        subject: "Pure Mathematics",
+        grade: "II",
+      },
+      {
+        subject: "Applied Mathematics",
+        grade: "I",
+      },
+      {
+        subject: "Physics",
+        grade: "II",
+      },
+      {
+        subject: "Integrated Mathematics",
+        grade: "I",
+      },
+      {
+        subject: "Communication Studies",
+        grade: "II",
+      },
+    ],
+  },
+  {
+    institution: "Trinity College East",
+    degree: "Caribbean Examination Council (CXC)",
+    start: "2010",
+    end: "2015",
+    location: "Trincity, Trinidad and Tobago",
+    grades: [
+      {
+        subject: "Mathematics",
+        grade: "I",
+      },
+      {
+        subject: "English A",
+        grade: "I",
+      },
+      {
+        subject: "English B",
+        grade: "II",
+      },
+      {
+        subject: "Information Technology",
+        grade: "I",
+      },
+      {
+        subject: "Physics",
+        grade: "I",
+      },
+      {
+        subject: "Chemistry",
+        grade: "I",
+      },
+      {
+        subject: "Biology",
+        grade: "I",
+      },
+      {
+        subject: "Principles of Business",
+        grade: "I",
+      },
+      {
+        subject: "Social Studies",
+        grade: "I",
+      },
+    ],
+  },
+];
 
-export const skills = [];
+export const skills = [
+  {
+    category: "Programming Languages",
+    items: [
+      "C",
+      "C++",
+      "Java",
+      "JavaScript",
+      "TypeScript",
+      "Python",
+      "SQL",
+      "MATLAB",
+      "VHDL",
+      "Dart",
+    ],
+  },
+  {
+    category: "Mobile Development",
+    items: ["Flutter"],
+  },
+  {
+    category: "Frontend Web Technologies",
+    items: [
+      "Angular",
+      "React",
+      "Svelte/SvelteKit",
+      "HTML",
+      "CSS",
+      "Thymeleaf",
+      "Tailwind CSS",
+      "Jinja2",
+      "JQuery",
+    ],
+  },
+  {
+    category: "Backend Web Technologies",
+    items: [
+      "Firebase",
+      "Supabase",
+      "Express JS",
+      "Spring Boot",
+      "Hono JS",
+      "Django",
+      "FastAPI",
+    ],
+  },
+  {
+    category: "Databases",
+    items: ["MongoDB", "Redis", "PostgreSQL", "Firebase Firestore"],
+  },
+  {
+    category: "Tools",
+    items: ["Docker", "Git", "GitHub Actions", "Visual Studio Code"],
+  },
+  {
+    category: "Operating Systems",
+    items: ["Linux", "Windows"],
+  },
+];
 
 export const certifications = [];
 
-export const workExperience = [];
+export const workExperience = [
+  {
+    company: "Agostini's Innovation Lab",
+    position: "Frontend Developer",
+    start: "November 2024",
+    end: "Present",
+    location: "Aranguez, Trinidad and Tobago",
+    infoPoints: [
+      `Assisted in the delivery of lectures, tutorials, and practical sessions for undergraduate students.`,
+      `Provided guidance and support to students, helping them to understand and apply course concepts.`,
+      `Assisted in the preparation, marking, and grading of assignments, tests, and exams.`,
+    ],
+  },
+  {
+    company: "Mondeum International Service Center",
+    position: "Junior Software Developer",
+    start: "August 2023",
+    end: "August 2024",
+    location: "Port-of-Spain, Trinidad and Tobago",
+    infoPoints: [
+      `Conducted thorough research and analysis to recommend and plan the development of new systems, ensuring they aligned with business goals and technical requirements.`,
+    ],
+  },
+  {
+    company: "Integrated Security Systems Ltd.",
+    position: "Technician Support",
+    start: "December 2018",
+    end: "August 2019",
+    location: "Barataria, Trinidad and Tobago",
+    infoPoints: [
+      `Assisted in the delivery of lectures, tutorials, and practical sessions for undergraduate students.`,
+      `Provided guidance and support to students, helping them to understand and apply course concepts.`,
+      `Assisted in the preparation, marking, and grading of assignments, tests, and exams.`,
+    ],
+  },
+  {
+    company: "Integrated Security Systems Ltd.",
+    position: "Customer Sales Representative",
+    start: "July 2018",
+    end: "August 2019",
+    location: "Barataria, Trinidad and Tobago",
+    infoPoints: [
+      `Assisted in the delivery of lectures, tutorials, and practical sessions for undergraduate students.`,
+      `Provided guidance and support to students, helping them to understand and apply course concepts.`,
+      `Assisted in the preparation, marking, and grading of assignments, tests, and exams.`,
+    ],
+  },
+];
 
 export const projects = [];
