f8b120767a4b047f9d1aa0486629c0e70ca6c283
Adrian Gookool <amgookool@hotmail.com>
2025-01-18 23:49:55 -0400

    ✨ Add Iconify icons support and update dependencies in package.json

--- apps/portfolio/tailwind.config.ts
+++ apps/portfolio/tailwind.config.ts
@@ -1,11 +1,15 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
+/* 
+Iconify Icons prefixes
+! https://github.com/iconify/icon-sets/blob/master/collections.md
+! Place in addIconSelectors functions
+*/
+import { addIconSelectors } from "@iconify/tailwind";
 
 export default {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
-} satisfies Config
-
-
+  plugins: [addIconSelectors(["logos", "devicon", "line-md"])],
+} satisfies Config;
